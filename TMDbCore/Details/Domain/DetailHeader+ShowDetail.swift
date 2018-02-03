d90ca05dd41a719141e618612afa65f6f4cc6a0f
Fernando Frances <fernando95frances@gmail.com>
2018-02-03 14:02:21 +0100

    Fixed Show Detail, Wasn't downloading because of a typing mistake in Endpoint class

--- TMDbCore/Details/Domain/DetailHeader+ShowDetail.swift
+++ TMDbCore/Details/Domain/DetailHeader+ShowDetail.swift
@@ -11,13 +11,11 @@ import Foundation
 extension DetailHeader {
     init(show: ShowDetail, dateFormatter: DateFormatter) {
         title = show.name
-        posterPath = nil //show.posterPath
-        backdropPath = nil //show.backdropPath
+        posterPath = show.posterPath
+        backdropPath = show.backdropPath
         
         let releaseDate = dateFormatter.date(from: show.releaseDate)
-        //let releaseDate = show.releaseDate.flatMap { dateFormatter.date(from: $0)}
         let year = (releaseDate?.year).map { String($0) }
-        let duration = "\(show.episodes) episodes."
         
         metadata = year!
     }
